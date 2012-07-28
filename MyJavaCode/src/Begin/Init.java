a1f6af7655939bd5bc7e4e44fba92cb62a5943a8
Rajnish Kumar <rajnish.iitg@gmail.com>
2012-07-29 05:20:43 +0530

    Beginning Java

---
+++ MyJavaCode/src/Begin/Init.java
@@ -0,0 +1,16 @@
+package Begin;
+
+public class Init {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Init" );
+		
+		TClass myClass = new TClass();
+		myClass.InitTClass();
+	}
+
+}
